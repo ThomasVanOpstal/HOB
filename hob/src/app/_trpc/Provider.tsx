523f3caa0f5a9eaf7f46c0c79f63893b61567b3a
ThomasVanOpstal <91283935+ThomasVanOpstal@users.noreply.github.com>
2024-02-06 14:49:52 +0100

    adding just the string

--- hob/src/app/_trpc/Provider.tsx
+++ hob/src/app/_trpc/Provider.tsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react'
 
 import { trpc } from './client'
 const url = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.PUBLIC_URL}/api/trpc`
+  ? `https://hob-ivory.vercel.app/api/trpc`
   : 'http://localhost:3000/api/trpc'
 
 export default function Provider({ children }: { children: React.ReactNode }) {
